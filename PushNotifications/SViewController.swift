59b79b83034a50a354e4c6091e3d9d46f931100f
Satyaa <satyaaiosdev@gmail.com>
2020-08-19 09:40:37 +0530

    Push Notifications

---
+++ PushNotifications/SViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SViewController.swift
+//  PushNotifications
+//
+//  Created by Satyaa Akana on 17/08/20.
+//  Copyright © 2020 Satyaa Akana. All rights reserved.
+//
+
+import UIKit
+
+class SViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
